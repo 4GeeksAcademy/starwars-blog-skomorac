de84e5ae448b5dc415c05e9e3700ceb5622dd864
Admir Skomorac <admir.skomorac@outlook.com>
2024-04-01 21:42:21 +0000

    work in progress, styling

--- src/js/component/mainCard.js
+++ src/js/component/mainCard.js
@@ -1,23 +1,70 @@
-import React from "react";
-import { Card, Carousel } from "react-bootstrap"; // Import Carousel component from React Bootstrap
+import React, { useState, useEffect } from "react";
+import { Card, Carousel, Container } from "react-bootstrap";
 
 const MainCard = ({ data, imageUrlKey, nameKey }) => {
+  const [numCards, setNumCards] = useState(5); // Default to 5 cards
+
+  useEffect(() => {
+    const handleResize = () => {
+      const width = window.innerWidth;
+      if (width < 680) {
+        setNumCards(1);
+      } else if (width >= 680 && width < 960) {
+        setNumCards(3);
+      } else {
+        setNumCards(5);
+      }
+    };
+
+    handleResize(); // Call initially to set initial number of cards
+
+    window.addEventListener("resize", handleResize); // Listen for resize events
+
+    return () => {
+      window.removeEventListener("resize", handleResize); // Clean up event listener
+    };
+  }, []); // Empty dependency array ensures this effect runs only once on mount
+
+  // Function to split data into chunks of numCards elements
+  const chunkArray = (arr, chunkSize) => {
+    return Array.from(
+      { length: Math.ceil(arr.length / chunkSize) },
+      (_, index) => arr.slice(index * chunkSize, index * chunkSize + chunkSize)
+    );
+  };
+
+  // Split data into chunks of numCards elements
+  const chunkedData = chunkArray(data, numCards);
+
   return (
-    <Carousel>
-      {data.map((item) => (
-        <Carousel.Item key={item.uid}>
-          <img
-            className="d-block w-100"
-            src={`https://starwars-visualguide.com/assets/img/${imageUrlKey}/${item.uid}.jpg`}
-            alt={item[nameKey]}
-          />
-          <Carousel.Caption>
-            <h3>{item[nameKey]}</h3>
-            {/* Add other details as needed */}
-          </Carousel.Caption>
-        </Carousel.Item>
-      ))}
-    </Carousel>
+    <Container fluid>
+      <Carousel className="h-100" interval={null}>
+        {" "}
+        {/* Set interval to null */}
+        {chunkedData.map((chunk, index) => (
+          <Carousel.Item key={index}>
+            <div className="d-flex justify-content-around h-100">
+              {chunk.map((item, i) => (
+                <div key={i} className="m-2 d-flex align-items-stretch">
+                  <Card className="h-100 bg-secondary">
+                    <Card.Img
+                      variant="top"
+                      src={`https://starwars-visualguide.com/assets/img/${imageUrlKey}/${item.uid}.jpg`}
+                      alt={item[nameKey]}
+                      className="h-70" // Adjust image height
+                    />
+                    <Card.Body className="d-flex flex-column">
+                      <Card.Title>{item[nameKey]}</Card.Title>
+                      {/* Add other details as needed */}
+                    </Card.Body>
+                  </Card>
+                </div>
+              ))}
+            </div>
+          </Carousel.Item>
+        ))}
+      </Carousel>
+    </Container>
   );
 };
 
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -6,8 +6,10 @@ export const Home = () => {
   const { store } = useContext(Context);
 
   return (
-    <div className="text-center mt-5">
+    <div className="text-center mt-5 bg-dark">
       <MainCard data={store.people} imageUrlKey="characters" nameKey="name" />
+      <MainCard data={store.vehicles} imageUrlKey="vehicles" nameKey="name" />
+      <MainCard data={store.planets} imageUrlKey="planets" nameKey="name" />
     </div>
   );
 };
