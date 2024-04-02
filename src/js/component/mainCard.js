538f13294b22efbecabed63498a8e0105ba4b276
Admir Skomorac <admir.skomorac@outlook.com>
2024-04-02 14:00:03 +0000

    preparing for chaos

--- src/js/component/mainCard.js
+++ src/js/component/mainCard.js
@@ -1,69 +1,28 @@
-import React, { useState, useEffect } from "react";
-import { Card, Carousel, Container } from "react-bootstrap";
+import React from "react";
+import { Card, Container } from "react-bootstrap";
+import "../../styles/index.css";
 
 const MainCard = ({ data, imageUrlKey, nameKey }) => {
-  const [numCards, setNumCards] = useState(5); // Default to 5 cards
-
-  useEffect(() => {
-    const handleResize = () => {
-      const width = window.innerWidth;
-      if (width < 680) {
-        setNumCards(1);
-      } else if (width >= 680 && width < 960) {
-        setNumCards(3);
-      } else {
-        setNumCards(5);
-      }
-    };
-
-    handleResize(); // Call initially to set initial number of cards
-
-    window.addEventListener("resize", handleResize); // Listen for resize events
-
-    return () => {
-      window.removeEventListener("resize", handleResize); // Clean up event listener
-    };
-  }, []); // Empty dependency array ensures this effect runs only once on mount
-
-  // Function to split data into chunks of numCards elements
-  const chunkArray = (arr, chunkSize) => {
-    return Array.from(
-      { length: Math.ceil(arr.length / chunkSize) },
-      (_, index) => arr.slice(index * chunkSize, index * chunkSize + chunkSize)
-    );
-  };
-
-  // Split data into chunks of numCards elements
-  const chunkedData = chunkArray(data, numCards);
-
   return (
     <Container fluid>
-      <Carousel className="h-100" interval={null}>
-        {" "}
-        {/* Set interval to null */}
-        {chunkedData.map((chunk, index) => (
-          <Carousel.Item key={index}>
-            <div className="d-flex justify-content-around h-100">
-              {chunk.map((item, i) => (
-                <div key={i} className="m-2 d-flex align-items-stretch">
-                  <Card className="h-100 bg-secondary">
-                    <Card.Img
-                      variant="top"
-                      src={`https://starwars-visualguide.com/assets/img/${imageUrlKey}/${item.uid}.jpg`}
-                      alt={item[nameKey]}
-                      className="h-70" // Adjust image height
-                    />
-                    <Card.Body className="d-flex flex-column">
-                      <Card.Title>{item[nameKey]}</Card.Title>
-                      {/* Add other details as needed */}
-                    </Card.Body>
-                  </Card>
-                </div>
-              ))}
-            </div>
-          </Carousel.Item>
+      <h1 className="mainTitle mb-3">{imageUrlKey}</h1>
+      <div className="card-container mb-5">
+        {data.map((item, i) => (
+          <div key={i} className="m-2">
+            <Card className="bg-secondary">
+              <Card.Img
+                variant="top"
+                src={`https://starwars-visualguide.com/assets/img/${imageUrlKey}/${item.uid}.jpg`}
+                alt={item[nameKey]}
+                className="card-image"
+              />
+              <Card.Body>
+                <Card.Title>{item[nameKey]}</Card.Title>
+              </Card.Body>
+            </Card>
+          </div>
         ))}
-      </Carousel>
+      </div>
     </Container>
   );
 };
