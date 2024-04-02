0b01aa391f5ae6951f94432b9ec935724da6fc57
Admir Skomorac <admir.skomorac@outlook.com>
2024-04-02 20:56:21 +0000

    nastavljamo sutra

---
+++ src/js/component/cardPeople.js
@@ -0,0 +1,12 @@
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+
+export const CardPeople = () => {
+  const { store, actions } = useContext(Context);
+
+  return (
+    <div>
+      <h1></h1>
+    </div>
+  );
+};
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,17 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import MainCard from "../component/mainCard";
+// import MainCard from "../component/mainCard";
+import { CardPeople } from "../component/cardPeople";
 
 export const Home = () => {
   const { store } = useContext(Context);
 
   return (
     <div className="text-center mt-5 bg-dark">
-      <MainCard data={store.people} imageUrlKey="characters" nameKey="name" />
+      {/* <MainCard data={store.people} imageUrlKey="characters" nameKey="name" />
       <MainCard data={store.vehicles} imageUrlKey="vehicles" nameKey="name" />
-      <MainCard data={store.planets} imageUrlKey="planets" nameKey="name" />
+      <MainCard data={store.planets} imageUrlKey="planets" nameKey="name" /> */}
+      <CardPeople />
     </div>
   );
 };
